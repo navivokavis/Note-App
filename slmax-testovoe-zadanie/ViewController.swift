8633ac772187d8e86bf5bcebe4fe946057ac2a9b
navivokavis <navivokavis@gmail.com>
2022-12-02 11:21:00 +0100

    Initial Commit

---
+++ slmax-testovoe-zadanie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  slmax-testovoe-zadanie
+//
+//  Created by Navi Vokavis on 2.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
